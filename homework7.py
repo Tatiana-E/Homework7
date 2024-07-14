3d8166aa2c3aa1c8aaa9a4317c4383b29676629a
Tatiana-E <tanya.era1989@gmail.com>
2024-07-14 21:08:41 +0500

    Update homework7.py

--- homework7.py
+++ homework7.py
@@ -1,8 +1,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students1=list(students)
-students1.sort()
-print(sum(grades[0])/len(grades[0]))
+students1.sort() 
 sredny_bal={students1[0]:sum(grades[0])/len(grades[0]),
             students1[1]:sum(grades[1])/len(grades[1]),
             students1[2]:sum(grades[2])/len(grades[2]),
